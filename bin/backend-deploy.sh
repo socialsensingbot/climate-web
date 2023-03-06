3332af1304aade44e33369319e2d71cf07da1255
neilellis <hello@neilellis.me>
2023-03-06 19:25:47 +0000

    Dockerizing.

--- bin/backend-deploy.sh
+++ bin/backend-deploy.sh
@@ -5,13 +5,8 @@ export PATH=$PATH:$(pwd)
 cd ..
 export BRANCH=${GITHUB_REF##*/}
 wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x ./jq && mv jq ./bin
-npm install -g typescript
-AWS_APP_NAME=climate
-AWS_APP_ID=d1ozf96chfutrg
-AWS_APP_STAGE=dev
-amplify configure project -n $AWS_APP_NAME -i $AWS_APP_ID  -s $AWS_APP_STAGE
-amplify env checkout $AWS_APP_STAGE
-amplify pull --appId $AWS_APP_ID --envName $AWS_APP_STAGE
+amplify env checkout dev
+
 if [[ "${BRANCH}" == staging ]]; then
   backup.sh
   amplify push -y
